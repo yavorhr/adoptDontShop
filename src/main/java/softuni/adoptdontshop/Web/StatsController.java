22db0cf5fa7f3f502715bc0e2a6a04bbbcb40b51
Yavor Hristozov <yavorhristozov@gmail.com>
2021-12-10 20:33:16 +0200

    Stats logic implement. Not finished.

--- src/main/java/softuni/adoptdontshop/Web/StatsController.java
+++ src/main/java/softuni/adoptdontshop/Web/StatsController.java
@@ -1,34 +1,28 @@
 package softuni.adoptdontshop.Web;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import softuni.adoptdontshop.Config.GlobalExceptionHandler;
-import softuni.adoptdontshop.Model.Model.BindingModel.DogAddBindingModel;
+import softuni.adoptdontshop.Service.DogService;
 import softuni.adoptdontshop.Service.StatsService;
-import softuni.adoptdontshop.Web.exception.GlobalNotFoundException;
-import softuni.adoptdontshop.Web.exception.ResourceNotFoundException;
 
 
 @Controller
 public class StatsController {
 
     private final StatsService statsService;
+    private final DogService dogService;
 
-    public StatsController(StatsService statsService) {
+    public StatsController(StatsService statsService, DogService dogService) {
         this.statsService = statsService;
+        this.dogService = dogService;
     }
 
     @GetMapping("/admin")
     public ModelAndView statistics() {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.addObject("stats", statsService.getStats());
+//        modelAndView.addObject("adoptedDogs",)
         modelAndView.setViewName("admin-stats");
         return modelAndView;
     }
